59a247aa76aef98669a62bf8ad84a9fcea30c99e
Maksim Fadeev <fadeevmaksim701@gmail.com>
2025-11-22 11:37:40 +0300

    Set up main menu flow

---
+++ Assets/_Project/_Develop/Runtime/MainMenu/MainMenuFlow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using VContainer.Unity;
+
+namespace TestTankProject.Runtime.MainMenu
+{
+    public class MainMenuFlow : IStartable
+    {
+        public void Start()
+        {
+            Debug.Log("MainMenuFlow start");
+        }
+    }
+}
